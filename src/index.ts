ece98b749fc5fa5cb3f27c2faed5bf4230cb8c46
Mohamed-Magdy-M <mmagdy@identity-solutions.org>
2022-10-14 18:11:20 +0200

    Add setup Express and Nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World");
+import express from "express";
+
+// Use express to setup a server
+// Using https://expressjs.com/en/starter/hello-world.html
+const app = express();
+// Set port to listen to when start server.
+const port = 3000;
+
+// Add a basic get request and send string to test the simple get request.
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Listen to this port and set a console message when starting server to test the server connection.
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
